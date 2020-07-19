cd6950a5db35446cfbaad7e4a69d75f3ee94c038
Ruilong <ruilong96@gmail.com>
2020-07-19 12:31:37 -0700

    initialize eureka server for instance

---
+++ src/main/java/market/trend/App.java
@@ -0,0 +1,13 @@
+package market.trend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
