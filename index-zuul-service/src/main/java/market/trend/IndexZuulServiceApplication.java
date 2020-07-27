c241f7d7fc8cfba0dc54c38f01fe3c3a112caa8f
Ruilong <ruilong96@gmail.com>
2020-07-26 17:18:27 -0700

    add proxy to balance the visits over different index code instances

---
+++ index-zuul-service/src/main/java/market/trend/IndexZuulServiceApplication.java
@@ -0,0 +1,26 @@
+package market.trend;
+ 
+import cn.hutool.core.util.NetUtil;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+  
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class IndexZuulServiceApplication {
+//  http://127.0.0.1:8031/api-codes/codes
+    public static void main(String[] args) {
+        int port = 8031;
+        if(!NetUtil.isUsableLocalPort(port)) {
+            System.err.printf("port %d is occupied，unable to start%n", port );
+            System.exit(1);
+        }
+        new SpringApplicationBuilder(IndexZuulServiceApplication.class).properties("server.port=" + port).run(args);
+  
+    }
+ 
+}
